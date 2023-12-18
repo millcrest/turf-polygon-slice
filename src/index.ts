6f27fd5cd81fc04213393f9a258cd4dc4262b30e
Corey Womack <corey.womack@millcrest.com.au>
2023-12-18 10:54:17 +0800

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,220 @@
-// Main library exports - these are packaged in your distributable
-export const isOdd = (n: number): boolean => {
-  return !!(n & 1);
-};
+import booleanPointInPolygon from "@turf/boolean-point-in-polygon"
+import difference from "@turf/difference"
+import {
+	Feature,
+	FeatureCollection,
+	LineString,
+	MultiPolygon,
+	Polygon,
+	Properties,
+	featureCollection,
+	lineString,
+	multiPolygon,
+	point,
+	polygon,
+} from "@turf/helpers"
+import { getGeom } from "@turf/invariant"
+import lineIntersect from "@turf/line-intersect"
+import lineOffset from "@turf/line-offset"
+import lineOverlap from "@turf/line-overlap"
+import lineToPolygon from "@turf/line-to-polygon"
+import unkinkPolygon from "@turf/unkink-polygon"
+
+/**
+ * Slices {@link Polygon} using a {@link Linestring}.
+ *
+ * @name polygonSlice
+ * @param {Feature<Polygon>} poly Polygon to slice
+ * @param {Feature<LineString>} splitter LineString used to slice Polygon
+ * @returns {FeatureCollection<Polygon>} Sliced Polygons
+ * @example
+ * var polygon = {
+ *   "geometry": {
+ *     "type": "Polygon",
+ *     "coordinates": [[
+ *         [0, 0],
+ *         [0, 10],
+ *         [10, 10],
+ *         [10, 0],
+ *         [0, 0]
+ *     ]]
+ *   }
+ * };
+ * var linestring =  {
+ *     "type": "Feature",
+ *     "properties": {},
+ *     "geometry": {
+ *       "type": "LineString",
+ *       "coordinates": [
+ *         [5, 15],
+ *         [5, -15]
+ *       ]
+ *     }
+ *   }
+ * var sliced = polygonSlice(polygon, linestring);
+ * //=sliced
+ */
+export default function polygonSlice(
+	poly: Feature<Polygon, Properties>,
+	splitter: Feature<LineString, Properties>,
+): FeatureCollection<Polygon> {
+	const polyGeom = getGeom(poly)
+	const splitterGeom = getGeom(splitter)
+
+	const line = trimStartEndPoints(polyGeom, splitterGeom)
+	if (line == null) return featureCollection([poly])
+
+	const newPolygons = []
+
+	const upperCut = cutPolygon(polyGeom, line, 1, "upper")
+	const lowerCut = cutPolygon(polyGeom, line, -1, "lower")
+	if (upperCut != null && lowerCut != null) {
+		newPolygons.push(upperCut.geometry)
+		newPolygons.push(lowerCut.geometry)
+	} else {
+		newPolygons.push(polyGeom)
+	}
+
+	const generatedPolygons: Polygon[] = []
+	newPolygons.forEach((polyg) => {
+		if (polyg.type == "Polygon") {
+			generatedPolygons.push(polyg)
+		}
+		if (polyg.type == "MultiPolygon") {
+			polyg.coordinates.forEach((p) => {
+				generatedPolygons.push(polygon([p[0]]).geometry)
+			})
+		}
+	})
+
+	return featureCollection(
+		generatedPolygons.map((p) => polygon(p.coordinates)),
+	)
+}
+
+function cutPolygon(
+	poly: Polygon,
+	line: LineString,
+	direction: number,
+	id: string,
+) {
+	let j
+	const cutPolyGeoms = []
+	let retVal = null
+
+	if (poly.type != "Polygon" || line.type != "LineString") return retVal
+
+	const intersectPoints = lineIntersect(poly, line)
+	const nPoints = intersectPoints.features.length
+	if (nPoints == 0 || nPoints % 2 != 0) return retVal
+
+	const thickLinePolygon = prepareDiffLinePolygon(line, direction)
+	if (!thickLinePolygon) return null
+
+	let clipped: Feature<Polygon | MultiPolygon> | null
+	try {
+		clipped = difference(poly, thickLinePolygon)
+	} catch (e) {
+		return retVal
+	}
+	if (!clipped) return null
+
+	if (clipped.geometry.type == "MultiPolygon") {
+		for (j = 0; j < clipped.geometry.coordinates.length; j++) {
+			const polyg = polygon(clipped.geometry.coordinates[j])
+			const overlap = lineOverlap(polyg, line, {
+				tolerance: 0.00005,
+			})
+
+			if (overlap.features.length > 0) {
+				cutPolyGeoms.push(polyg.geometry.coordinates)
+			}
+		}
+	} else {
+		const polyg = polygon(clipped.geometry.coordinates)
+		const overlap = lineOverlap(polyg, line, { tolerance: 0.00005 })
+
+		if (overlap.features.length > 0) {
+			cutPolyGeoms.push(polyg.geometry.coordinates)
+		}
+	}
+
+	if (cutPolyGeoms.length == 1) {
+		retVal = polygon(cutPolyGeoms[0], { id: id })
+	} else if (cutPolyGeoms.length > 1) {
+		retVal = multiPolygon(cutPolyGeoms, { id: id })
+	}
+
+	return retVal
+}
+/**
+ * return non self intersection polygon
+ * for difference-cutting
+ */
+function prepareDiffLinePolygon(line: LineString, direction: number) {
+	let j,
+		k,
+		offsetLine,
+		polyCoords = []
+	let thickLinePolygon: Feature<Polygon, Properties> | null = null
+
+	const offsetScales = [0.01, 0.001, 0.0001]
+
+	for (j = 0; j < offsetScales.length; j++) {
+		polyCoords = []
+		offsetLine = lineOffset(line, offsetScales[j] * direction, {
+			units: "kilometers",
+		})
+		for (k = 0; k < line.coordinates.length; k++) {
+			polyCoords.push(line.coordinates[k])
+		}
+		for (k = offsetLine.geometry.coordinates.length - 1; k >= 0; k--) {
+			polyCoords.push(offsetLine.geometry.coordinates[k])
+		}
+		polyCoords.push(line.coordinates[0])
+		const thickLineString = lineString(polyCoords)
+		thickLinePolygon = lineToPolygon(thickLineString) as Feature<
+			Polygon,
+			Properties
+		>
+
+		const result = unkinkPolygon(thickLinePolygon)
+
+		const selfIntersectPolygons = result.features.length
+
+		if (selfIntersectPolygons == 1) {
+			return thickLinePolygon
+		}
+	}
+	return thickLinePolygon
+}
+
+/**
+ * Prepare linestrings from polygon-cut
+ * avoid start and end points inside polygon for calculation
+ */
+function trimStartEndPoints(poly: Polygon, line: LineString) {
+	let j
+	let startAt = 0
+	let endAt = line.coordinates.length
+
+	for (j = 0; j < line.coordinates.length; j++) {
+		if (booleanPointInPolygon(point(line.coordinates[j]), poly)) {
+			startAt++
+		} else {
+			break
+		}
+	}
+
+	for (j = line.coordinates.length - 1; j >= 0; j--) {
+		if (booleanPointInPolygon(point(line.coordinates[j]), poly)) {
+			endAt--
+		} else {
+			break
+		}
+	}
+
+	line.coordinates = line.coordinates.slice(startAt, endAt)
+
+	return line.coordinates.length > 1 ? line : null
+}
